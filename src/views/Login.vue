9f0e6c8630989e074fd31ae2136796f5bbbec557
wxamlzm <zd5024@163.com>
2021-09-14 21:01:36 +0800

    UPDATE:尝试增加登录页面准备

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="Login">
+        <div class="wrap"></div>
+        <div class="sign-form"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'login'
+}
+</script>
+<style lang="scss">
+    @import '@/assets/css/login.scss'
+</style>
